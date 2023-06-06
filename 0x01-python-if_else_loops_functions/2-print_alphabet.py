c9344caf41b55f9cf838ada75e1b47302d396265
imansai <123962418+imansai@users.noreply.github.com>
2023-06-06 23:11:18 +0100

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for ascii_code in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_code), end='')
-
-print()
+alphabet = [chr(char_code) for char_code in range(ord('a'), ord('z') + 1)]
+alphabet_string = ''.join(alphabet)
+print(alphabet_string)
