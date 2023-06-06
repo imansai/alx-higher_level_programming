8b78b88f1ee55af539633383b8a9c113ca61e0cd
imansai <123962418+imansai@users.noreply.github.com>
2023-06-06 22:34:15 +0100

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
 num_str = str(number)
 last_num = num_str[-1]
-int_last_num = int(last_num)
 
 if number >= 0:
+    int_last_num = int(last_num)
     if int_last_num == 0:
-        print(f"Last digit of {number} is {int_last_num} and is 0")
+        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is 0")
     elif int_last_num < 6 and int_last_num != 0:
-        print(f"Last digit of {number} is {int_last_num} and is less than 6 and not 0")
+        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is less than 6 and not 0")
     elif int_last_num > 5:
-        print(f"Last digit of {number} is {int_last_num} and is greater than 5")
+        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is greater than 5")
 else:
-    int_last_num *= -1
+    int_last_num = int(last_num) * -1
     if int_last_num == 0:
-        print(f"Last digit of {number} is {int_last_num} and is 0")
+        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is 0")
     elif int_last_num < 6 and int_last_num != 0:
-        print(f"Last digit of {number} is {int_last_num} and is less than 6 and not 0")
+        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is less than 6 and not 0")
     elif int_last_num > 5:
-        print(f"Last digit of {number} is {int_last_num} and is greater than 5")
+        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is greater than 5")
