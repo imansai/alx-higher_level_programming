cac8213642af8e312dae392429085de7eb08287f
imansai <123962418+imansai@users.noreply.github.com>
2023-06-06 22:41:42 +0100

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,16 +7,22 @@ last_num = num_str[-1]
 if number >= 0:
     int_last_num = int(last_num)
     if int_last_num == 0:
-        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is 0")
+        print("Last digit of " + str(number) + " is "
+              + str(int_last_num) + " and is 0")
     elif int_last_num < 6 and int_last_num != 0:
-        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is less than 6 and not 0")
+        print("Last digit of " + str(number) + " is "
+              + str(int_last_num) + " and is less than 6 and not 0")
     elif int_last_num > 5:
-        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is greater than 5")
+        print("Last digit of " + str(number) + " is "
+              + str(int_last_num) + " and is greater than 5")
 else:
     int_last_num = int(last_num) * -1
     if int_last_num == 0:
-        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is 0")
+        print("Last digit of " + str(number) + " is "
+              + str(int_last_num) + " and is 0")
     elif int_last_num < 6 and int_last_num != 0:
-        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is less than 6 and not 0")
+        print("Last digit of " + str(number) + " is "
+              + str(int_last_num) + " and is less than 6 and not 0")
     elif int_last_num > 5:
-        print("Last digit of " + str(number) + " is " + str(int_last_num) + " and is greater than 5")
+        print("Last digit of " + str(number) + " is "
+              + str(int_last_num) + " and is greater than 5")
