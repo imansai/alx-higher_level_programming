94d636cb770bd104e957f6a016c48d819b8dd9bd
imansai <123962418+imansai@users.noreply.github.com>
2023-06-07 01:12:59 +0100

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
 
-"""Prints the ASCII alphabet, in lowercase, not followed by a new line."""
 for alphab in range(97, 123):
     print("{}".format(chr(alphab)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 for alphab in range(97, 123):
     if chr(alphab) is not 'q' and chr(alphab) is not 'e':
         print("{}".format(chr(alphab)), end="")
