19d97733c3398e4e47c830446967eaa09124c121
imansai <im.saibari@gmail.com>
2023-06-08 16:08:12 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     mul_result = mul(a, b)
     div_result = div(a, b)
 
-    print(add_result)
-    print(sub_result)
-    print(mul_result)
-    print(div_result)
+    print(f"{a} + {b} = {add_result}")
+    print(f"{a} - {b} = {sub_result}")
+    print(f"{a} * {b} = {mul_result}")
+    print(f"{a} / {b} = {div_result}")
