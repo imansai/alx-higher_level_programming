31f814abad42d05135a240e06c510a12b2d1a40a
imansai <im.saibari@gmail.com>
2023-06-12 15:53:58 -0700

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list):
     for i in range(len(my_list)):
-        number = my_list[i]
-        print("{}" .format(number))
+        print("{:d}" .format(my_list[i]))
