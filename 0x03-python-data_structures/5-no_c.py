334be5ac9b81fee1fcbc3588a21c2b17605937f4
imansai <im.saibari@gmail.com>
2023-06-12 18:16:56 -0700

    update

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+        if char.lower() != 'c' and char.lower() != 'C':
+            result += char
+    return result
