d30f7e0bb8282da1cc25b4e41ddbe78e65ca7cc7
imansai <im.saibari@gmail.com>
2023-06-26 19:28:20 -0700

    update

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
-
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a specified message.
-
-    Args:
-        message (str): The message to include in the exception (default: "").
-
-    Raises:
-        NameError: Always raises a NameError exception with the specified message.
-    """
+    """Raise a NameError exception with a message."""
     raise NameError(message)
