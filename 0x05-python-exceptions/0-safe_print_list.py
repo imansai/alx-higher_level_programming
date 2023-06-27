1835baf963a4531b44248669efcf5d0f08922f22
imansai <im.saibari@gmail.com>
2023-06-26 18:48:15 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             count += 1
             if count == x:
                 break
-    except Exception:
-        pass
+    except Exception as e:
+        print("An error occurred:", str(e))
     finally:
         print()
         return count
