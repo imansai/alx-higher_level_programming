7663dfc03d89a602ac87d8b03939cb52ff83e6ee
imansai <im.saibari@gmail.com>
2023-06-26 18:39:23 -0700

    update

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            print(item, end="")
+            count += 1
+            if count == x:
+                break
+    except:
+        pass
+    finally:
+        print()
+        return count
