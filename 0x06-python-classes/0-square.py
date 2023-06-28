fdc5489429a4e50637b3e55e4d8d6c2337fed7e9
imansai <im.saibari@gmail.com>
2023-06-28 12:37:32 -0700

    update

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defining empty class Square."""
+
+
+class Square:
+    """Defining a square."""
+    pass
+
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining empty class Square."""
+
+
+class Square:
+    """Defining square."""
+
+    def __init__(self, size):
+        """Initializing  new Square.
+
+        Args:
+            size (int): New square size.
+        """
+        self.__size = size
