97843907c8c876cf7a84588497f3efa480166fe0
imansai <im.saibari@gmail.com>
2023-08-16 16:58:55 -0700

    Correct task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
--- Create a new MySQL user 'user_0d_1'@'localhost' with the specified password 'user_0d_1_pwd', if the user doesn't already exist.
+-- Create the user 'user_0d_1'@'localhost' with all privileges and the specified password 'user_0d_1_pwd' if it doesn't already exist.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases and tables to the user 'user_0d_1' when connecting from the local machine ('localhost').
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Refresh the MySQL privilege cache to apply the recent privilege changes.
 FLUSH PRIVILEGES;
