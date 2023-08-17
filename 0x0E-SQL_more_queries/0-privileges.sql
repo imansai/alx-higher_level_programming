b192a6d86450237eec2728bf13e064cd2f3bbcce
imansai <im.saibari@gmail.com>
2023-08-16 17:05:24 -0700

    Correct task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Display the granted privileges for the user 'user_0d_1' and 'user_0d_2' when accessing from the local machine ('localhost').
+-- Show 'user_0d_1' and 'user_0d_2' privileges on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
