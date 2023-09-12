2b1f29ac2414b9d6acaea7eeffebc58819b39084
imansai <im.saibari@gmail.com>
2023-09-12 10:54:30 -0700

    Add solution 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 const num = Number(process.argv[2]);
 
-function factorial(num) {
+function factorial (num) {
   let result = 1;
-  for (let i = 0; i < num ; i++) {
-	result *= num-i;
+  if (!isNaN(num)) {
+    for (let i = 0; i < num; i++) {
+      result *= num - i;
+    }
+    console.log(result);
+  } else {
+    console.log(result);
   }
-  console.log(result);
 }
 
 factorial(num);
