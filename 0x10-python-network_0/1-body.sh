dcaf108d8aa93dfef167900b453248db339eb3c5
imansai <im.saibari@gmail.com>
2023-09-28 16:51:35 -0700

    Correct task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# Checks if  URL is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Ges URL from CL argument
-url="$1"
-response_headers=$(curl -sI "$url")
-http_status=$(echo "$response_headers" | grep -i "^HTTP" | awk '{print $2}')
-if [ "$http_status" -eq 200 ]; then
-    curl -sL "$url"
-else
-    echo "HTTP Status Code: $http_status"
-    echo "Failed to retrieve the URL: $url"
-    exit 1
-fi
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
