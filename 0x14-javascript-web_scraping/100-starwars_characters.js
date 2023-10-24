778c5d794797c6780afa52e190afeafd4e645833
imansai <im.saibari@gmail.com>
2023-10-24 03:13:28 -0700

    Update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
-const movieId = process.argv[2];
-const apiUrl = 'https://swapi.dev/api/films/';
-request.get(apiUrl + movieId, function (filmError, filmResponse, filmBody) {
-  if (filmError) {
-    console.log('Error fetching film details:', filmError);
-  } else {
-    const filmData = JSON.parse(filmBody);
-    const characterUrls = filmData.characters;
-    characterUrls.forEach(function (characterUrl) {
-      request.get(characterUrl, function (charError, charResponse, charBody) {
-        if (charError) {
-          console.log('Error fetching character details:', charError);
-        } else {
-          const characterData = JSON.parse(charBody);
-          console.log('Character Name:', characterData.name);
-        }
-      });
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
     });
   }
 });
-
