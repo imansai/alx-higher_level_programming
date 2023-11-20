94bde5d4ce66347f372d2a6dc7c9eb5d7b4b10b0
imansai <im.saibari@gmail.com>
2023-11-20 04:10:39 -0800

    Add function 5 with test file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a function that indents text."""
+
+
+def text_indentation(text):
+    """Print text with two lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): Text to be printed.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
