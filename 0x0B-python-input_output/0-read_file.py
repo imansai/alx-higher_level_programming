c3a40d9549eb37f53bbed946f4de09f66643d153
imansai <im.saibari@gmail.com>
2023-11-20 06:33:33 -0800

    Add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Defines a function that reads a file and prints."""
+
+
 def read_file(filename=""):
-    """Defines a function that reads a file and prints."""
+   """Prints UTF8 text content to stdout."""
     with open(filename, encoding='utf8') as f:
         print(f.read(), end='')
