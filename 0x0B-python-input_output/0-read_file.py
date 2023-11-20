f21a45a947915472a52c55b92bf0fc6c10f185b4
imansai <im.saibari@gmail.com>
2023-11-20 06:30:29 -0800

    Add file-reading function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """
-    Read the contents of the given text file (UTF8) and print it to stdout.
-
-    Args:
-        filename (str): The name of the file to read.
-
-    Returns:
-        None
-    """
-    with open(filename, 'r', encoding='utf8') as file:
-        content = file.read()
-        print(content, end='')
+    """Defines a function that reads a file and prints."""
+    with open(filename, encoding='utf8') as f:
+        print(f.read(), end='')
